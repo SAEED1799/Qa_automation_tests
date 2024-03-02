1951611fa1d6f90f4e99d8605eccdc31426899cf
saeedSAEED1799 <73116797+SAEED1799@users.noreply.github.com>
2024-03-02 20:28:58 +0200

    Update PrivatAreaPage.py

--- pythonProject5_Gullever/Logic/PrivatAreaPage.py
+++ pythonProject5_Gullever/Logic/PrivatAreaPage.py
@@ -3,21 +3,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from infra.base_page import BasePage
 
-class areaGulliver():
+
+class areaGulliver(BasePage):
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
+
 
     def click_ok_button_id(self):
-        order_ok_button = WebDriverWait(self.driver, 20).until(
+        order_ok_button = WebDriverWait(self._driver, 20).until(
             EC.visibility_of_element_located((By.XPATH,
                                               "/html/body/div[2]/div/div/div/div/div[3]/div[1]/div[1]"
                                               "/div/div/form/div/div[3]/button/span"))
         )
         order_ok_button.click()
 
-    def title_on_area(self):
-        package_title = WebDriverWait(self.driver, 20).until(
+    def title_on_area_is_displayed(self):
+        package_title = WebDriverWait(self._driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div/div[2]/p"))
         )
-        return package_title
+        return package_title.is_displayed()
