cd66d9d782de57fadfa870ed62540743b65260f6
saeedSAEED1799 <73116797+SAEED1799@users.noreply.github.com>
2024-03-02 20:28:29 +0200

    Update PackagesPage.py

--- pythonProject5_Gullever/Logic/PackagesPage.py
+++ pythonProject5_Gullever/Logic/PackagesPage.py
@@ -3,33 +3,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from infra.base_page import BasePage
 
-class PackagesGulliver():
+
+class PackagesGulliver(BasePage):
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
+
 
     def click_on_first_package(self):
         # cuntinue_buttons = self.driver.find_elements(By.XPATH, "//a[text()='המשך']")
         # print(cuntinue_buttons)
-        first_result_link = WebDriverWait(self.driver, 20).until(
+        first_result_link = WebDriverWait(self. _driver, 20).until(
             EC.visibility_of_element_located(
-                (By.XPATH, "//a[@class='order-btn' and @data-gamitee-link='https://www.gulliver.co.il/"
-                           "packages/details?PackageKey=alp_100502266368_16625678721"
-                           "_XDQAFY_2024-04-11_2024-04-14_HER_57120037_57119995_1_21640_2_0_0"
-                           "_TLV&NumOfAdults=2&NumOfChilds=0&NumOfInfants=0&Affiliate=gamitee']"))
+                (By.XPATH, "//*[@id='packages-results']/div[2]/div[2]/div[2]/div/generic-deals-group[1]"
+                           "/div[2]/generic-deal[1]/div/div[1]/div[2]/div[3]/div[6]/a"))
         )
         # cuntinue_buttons[1].click()
         first_result_link.click()
 
     def package_meaorgan_isDisplayed(self):
-        package_meaorgan = WebDriverWait(self.driver, 20).until(
+        package_meaorgan = WebDriverWait(self._driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, "//*[@id='interactiveMenu']/li[5]/a/span[2]"))
         )
         return package_meaorgan.is_displayed()
 
     def click_search_withoutdate(self):
         try:
-            hotelWorld_button = WebDriverWait(self.driver, 20).until(
+            hotelWorld_button = WebDriverWait(self._driver, 20).until(
                 EC.visibility_of_element_located(
                     (By.XPATH, "/html/body/div[2]/div/div/div/div/div[2]/div[1]/search-box/div/div/div[6]/input"))
             )
@@ -38,14 +39,14 @@ class PackagesGulliver():
             print(f"An error occurred: {e}")
 
     def package_withoyt_dates_isDisplayed(self):
-        package_n_date = WebDriverWait(self.driver, 20).until(
+        package_n_date = WebDriverWait(self._driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div"))
         )
         return package_n_date.is_displayed()
 
     def click_hotel_sale(self):
         try:
-            hotelWorld_button = WebDriverWait(self.driver, 20).until(
+            hotelWorld_button = WebDriverWait(self._driver, 20).until(
                 EC.visibility_of_element_located(
                     (By.XPATH, "/html/body/div[2]/div/div/div/div/div/sale-page-deals/div/div/div[2]/a[1]/div/div[3]"))
             )
@@ -54,13 +55,13 @@ class PackagesGulliver():
             print(f"An error occurred: {e}")
 
     def check_sale_page_isdisplayed(self):
-        package_n_sale = WebDriverWait(self.driver, 20).until(
+        package_n_sale = WebDriverWait(self._driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, "//div[contains(text(),'מחיר')]"))
         )
         return package_n_sale.is_displayed()
 
     def package_meaorgen_table_is_displayed(self):
-        package_n_sale = WebDriverWait(self.driver, 20).until(
+        package_n_sale = WebDriverWait(self._driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'חיפוש טיול מאורגן')]"))
         )
         return package_n_sale.is_displayed()
