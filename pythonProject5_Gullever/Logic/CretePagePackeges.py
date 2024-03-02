5a313e75605ff0b7a3d9824bd613c9b5e2989fe0
saeedSAEED1799 <73116797+SAEED1799@users.noreply.github.com>
2024-03-02 20:27:03 +0200

    Update CretePagePackeges.py

--- pythonProject5_Gullever/Logic/CretePagePackeges.py
+++ pythonProject5_Gullever/Logic/CretePagePackeges.py
@@ -3,12 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class CreteGulliver():
+from infra.base_page import BasePage
+
+
+class CreteGulliver(BasePage):
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
 
     def click_on_kritim(self):
-        kritim_package_link = WebDriverWait(self.driver, 20).until(
+        kritim_package_link = WebDriverWait(self._driver, 20).until(
             EC.visibility_of_element_located((By.XPATH, "//a[contains(text(),'כרתים')]"))
         )
         kritim_package_link.click()
